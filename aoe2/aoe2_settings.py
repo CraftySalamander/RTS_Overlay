c2ff7aa82692b422e6c40ff867abf6a97190a8ce
CraftySalamander <nico.vandernoot+1@gmail.com>
2024-03-09 11:38:06 +0100

    update default settings

--- aoe2/aoe2_settings.py
+++ aoe2/aoe2_settings.py
@@ -60,7 +60,7 @@ class AoE2OverlaySettings(RTSOverlaySettings):
         self.hotkeys = RTSTimerHotkeys()
 
         # timer speed factor
-        self.timer_speed_factor = 1.606
+        self.timer_speed_factor = 1.608
 
 
 if __name__ == '__main__':
