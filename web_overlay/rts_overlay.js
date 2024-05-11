277e32722c6a79f489450edfede32134faf1841f
CraftySalamander <nico.vandernoot+1@gmail.com>
2024-05-11 11:41:57 +0200

    clean JS calls

--- web_overlay/rts_overlay.js
+++ web_overlay/rts_overlay.js
@@ -1,25 +1,53 @@
-// Configuration parameters
-const SLEEP_TIME = 100;
+// -- Define parameters -- //
+const BO_IMAGE_HEIGHT = 30;  // Height of the images in the Build Order (BO).
+const ACTION_BUTTON_HEIGHT = 20;  // Height of the action buttons.
+const SLEEP_TIME = 100;           // Sleep time to resize the window [ms]
 
-// Variables
-let overlayWindow = null;
-let dataBO = null;
-let stepID = -1;
-let stepCount = -1;
+// -- Variables -- //
+let gameName = 'aoe2';     // Name of the game (i.e. its picture folder)
+let dataBO = null;         // Data of the selected BO
+let stepCount = -1;        // Number of steps of the current BO
+let stepID = -1;           // ID of the current BO step
+let overlayWindow = null;  // Window for the overlay
 
-// Limit a value in the [min ; max] range
+/**
+ * Sleep for a few ms
+ *
+ * @param {int} time_ms    Time to sleep [ms].
+ *
+ * @returns Function to sleep the requested time.
+ */
+function sleep(time_ms) {
+  return new Promise(resolve => setTimeout(resolve, time_ms));
+}
+
+/**
+ * Limit a value in the [min ; max] range
+ *
+ * @param {*} value    Value to limit.
+ * @param {*} min      Minimal bound.
+ * @param {*} max      Maximal bound.
+ *
+ * @returns 'value' limited in the [min ; max] range.
+ */
 function limitValue(value, min, max) {
   return (value <= min) ? min : (value >= max ? max : value);
 }
 
-// Sleep for a few ms
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+/**
+ * Limit the step ID to valid values.
+ */
+function limitStepID() {
+  if (stepCount < 1) {
+    stepID = -1;
+  } else {
+    stepID = limitValue(stepID, 0, stepCount - 1);
+  }
 }
 
 /**
- * Resize the overlay and move it to keep its top right corner at the same
- * position.
+ * Resize the overlay and move it to keep its top right corner
+ * at the same position.
  */
 function overlayResizeMove() {
   // Save upper right corner position
@@ -41,30 +69,27 @@ function overlayResizeMove() {
   });
 }
 
-// Limit the step ID to valid values
-function limitStepID() {
-  if (stepCount < 1) {
-    stepID = -1;
-  } else {
-    stepID = limitValue(stepID, 0, stepCount - 1);
-  }
-}
-
-// Previous BO step (Configuration window)
+/**
+ * Move to the previous BO step (configuration window).
+ */
 function previousStepConfig() {
   stepID--;
   limitStepID();
   updateBOPanel(false);
 }
 
-// Next BO step (Configuration window)
+/**
+ * Move to the next BO step (configuration window).
+ */
 function nextStepConfig() {
   stepID++;
   limitStepID();
   updateBOPanel(false);
 }
 
-// Previous BO step (Overlay window)
+/**
+ * Move to the previous BO step (overlay window).
+ */
 function previousStepOverlay() {
   stepID--;
   limitStepID();
@@ -72,7 +97,9 @@ function previousStepOverlay() {
   overlayResizeMove();
 }
 
-// Next BO step (Overlay window)
+/**
+ * Move to the next BO step (overlay window).
+ */
 function nextStepOverlay() {
   stepID++;
   limitStepID();
@@ -80,7 +107,11 @@ function nextStepOverlay() {
   overlayResizeMove();
 }
 
-// Basic template to work on a valid BO
+/**
+ * Get a basic template to work on a valid BO.
+ *
+ * @returns Requested BO.
+ */
 function getTemplateBO() {
   return `
   {
@@ -287,7 +318,13 @@ function splitNoteLine(noteLine) {
   return lineSplit
 }
 
-// Check if an image exist.
+/**
+ * Check if an image exist.
+ *
+ * @param {string} imageStr    Image to check (with path and extension).
+ *
+ * @returns True if valid image.
+ */
 function checkImageExist(imageStr) {
   if (!imageStr) {
     return false;
@@ -300,12 +337,13 @@ function checkImageExist(imageStr) {
 /**
  * Get the path for an image.
  *
- * @param {string} imageSearch    Image to search.
+ * @param {string} imageSearch    Image to search, with extension and path
+ *                                starting in 'common' or 'game' picture folder.
  *
- * @returns Image with its path, None if not found.
+ * @returns Image with its path, 'null' if not found.
  */
 function getImagePath(imageSearch) {
-  const gamePicturesFolder = '../pictures/aoe2';
+  const gamePicturesFolder = '../pictures/' + gameName;
   const commonPicturesFolder = '../pictures/common';
 
   if (gamePicturesFolder != null)  // try first with the game folder
@@ -329,72 +367,133 @@ function getImagePath(imageSearch) {
 }
 
 /**
+ * Get the HTML code to add an image.
+ *
+ * @param {*} imagePath       Image to display (with path and extension).
+ * @param {*} imageHeight     Height of the image.
+ * @param {*} functionName    Name of the function to call when clicking on the
+ *                            image, null if no function to call.
+ *
+ * @returns Requested HTML code.
+ */
+function getImageHTML(imagePath, imageHeight, functionName = null) {
+  if (functionName) {
+    return '<input type="image" src="' + imagePath + '" height="' +
+        imageHeight + '" onclick="' + functionName + '()"/>';
+  } else {
+    return '<input type="image" src="' + imagePath + '" height="' +
+        imageHeight + '"/>';
+  }
+}
+
+/**
+ * Check if the BO is valid and updates the BO variables if not valid.
+ *
+ * @returns True if valid BO.
+ */
+function checkValidBO() {
+  // Valid BO
+  if (dataBO && (stepCount >= 1) && (stepID >= 0) && (stepID < stepCount)) {
+    return true;
+  }
+
+  // Invalid BO
+  dataBO = null;
+  stepCount = -1;
+  stepID = -1;
+
+  return false;
+}
+
+/**
  * Get the content of the BO panel.
  *
- * @param {bool} overlayFlag    True for overlay, false for configuration
- *     window.
+ * @param {bool} overlayFlag    True for overlay, false for
+ *                              configuration window.
  * @param {int} idBO            Requested ID for the BO.
  *
  * @returns String representing the HTML part of the BO panel.
  */
 function getBOPanelContent(overlayFlag, idBO) {
-  let htmlBOString = '<!-- Configuration from within the BO panel -->';
-
-  if (!dataBO || (idBO < 0) || (idBO >= stepCount)) {
-    return htmlBOString +
-        '<nobr><div class="bo_line">Not valid BO</div></nobr>';
+  // Check if BO is valid
+  if (!checkValidBO()) {
+    return '<nobr><div class="bo_line">The build order is not valid.</div></nobr>';
   }
 
-  htmlBOString += '<nobr><div class="bo_line bo_line_config">0:00';
+  // Prepare HTML content for the BO body
+  let htmlString = '';
 
+  // Folders with requested pictures
+  const gamePicturesFolder = '../pictures/' + gameName + '/';
+  const commonPicturesFolder = '../pictures/common/';
+
+  // Configuration from within the BO panel
+  htmlString += '<nobr><div class="bo_line bo_line_config">';
+
+  const timingFlag = false;  // TODO Implement time function
+
+  // Current step or time
+  htmlString += timingFlag ? '0:00' : 'Step: ' + (idBO + 1) + '/' + stepCount;
+
+  // Previous or next step
+  const stepFunctionSuffix = overlayFlag ? 'Overlay' : 'Config';
+
+  htmlString += getImageHTML(
+      commonPicturesFolder + 'action_button/previous.png', ACTION_BUTTON_HEIGHT,
+      'previousStep' + stepFunctionSuffix);
+  htmlString += getImageHTML(
+      commonPicturesFolder + 'action_button/next.png', ACTION_BUTTON_HEIGHT,
+      'nextStep' + stepFunctionSuffix);
+
+  // Update timer
+  if (timingFlag) {
+    htmlString += getImageHTML(
+        commonPicturesFolder + 'action_button/start_stop_active.png',
+        ACTION_BUTTON_HEIGHT);
+    htmlString += getImageHTML(
+        commonPicturesFolder + 'action_button/timer_0.png',
+        ACTION_BUTTON_HEIGHT);
+  }
+
+  // Switch between manual and timer
   if (overlayFlag) {
-    htmlBOString += `
-    <input type="image" src="../pictures/common/action_button/previous.png" height="20" onclick="previousStepOverlay()"/>
-    <input type="image" src="../pictures/common/action_button/next.png" height="20" onclick="nextStepOverlay()"/>
-    `;
-  } else {
-    htmlBOString += `
-    <input type="image" src="../pictures/common/action_button/previous.png" height="20" onclick="previousStepConfig()"/>
-    <input type="image" src="../pictures/common/action_button/next.png" height="20" onclick="nextStepConfig()"/>
-    `;
+    htmlString += getImageHTML(
+        commonPicturesFolder + 'action_button/manual_timer_switch.png',
+        ACTION_BUTTON_HEIGHT);
   }
+  htmlString += '</div></nobr>';
 
-  htmlBOString += `
-    <img src="../pictures/common/action_button/start_stop_active.png" height="20"> <img
-          src="../pictures/common/action_button/timer_0.png" height="20">
-          <img src="../pictures/common/action_button/manual_timer_switch.png"
-          height="20">
-  </div></nobr>
-  `;
+  // Resources
+  const currentStep = dataBO.build_order[idBO];
+  const resources = currentStep.resources;
 
-  htmlBOString += '<!-- Resources indication --><div>';
+  htmlString += '<div>';
 
-  currentStep = dataBO.build_order[stepID];
-  resources = currentStep.resources;
+  htmlString += '<nobr><div class="bo_line bo_line_resources">';
 
-  htmlBOString += '<nobr><div class="bo_line bo_line_resources">';
-
-  htmlBOString +=
-      '<img src="../pictures/aoe2/resource/Aoe2de_wood.png" height="30" />' +
+  const resourceFolder = gamePicturesFolder + 'resource/';
+  htmlString +=
+      getImageHTML(resourceFolder + 'Aoe2de_wood.png', BO_IMAGE_HEIGHT) +
       resources.wood;
 
-  htmlBOString +=
-      '<img src="../pictures/aoe2/resource/Aoe2de_food.png" height="30" />' +
+  htmlString +=
+      getImageHTML(resourceFolder + 'Aoe2de_food.png', BO_IMAGE_HEIGHT) +
       resources.food;
 
-  htmlBOString +=
-      '<img src="../pictures/aoe2/resource/Aoe2de_gold.png" height="30" />' +
+  htmlString +=
+      getImageHTML(resourceFolder + 'Aoe2de_gold.png', BO_IMAGE_HEIGHT) +
       resources.gold;
 
-  htmlBOString +=
-      '<img src="../pictures/aoe2/resource/Aoe2de_stone.png" height="30" />' +
+  htmlString +=
+      getImageHTML(resourceFolder + 'Aoe2de_stone.png', BO_IMAGE_HEIGHT) +
       resources.stone;
 
-  htmlBOString +=
-      '<img src="../pictures/aoe2/resource/MaleVillDE_alpha.png" height="30" />' +
+  htmlString +=
+      getImageHTML(resourceFolder + 'MaleVillDE_alpha.png', BO_IMAGE_HEIGHT) +
       currentStep.villager_count;
 
-  let ageImage = 'AgeUnknown.png';
+  // Age image
+  let ageImage = null;
 
   switch (currentStep.age) {
     case 1:
@@ -410,56 +509,74 @@ function getBOPanelContent(overlayFlag, idBO) {
       ageImage = 'ImperialAgeIconDE_alpha.png';
       break;
     default:
-      ageImage = 'AgeUnknown.png';
+      ageImage = null;
   }
 
-  htmlBOString +=
-      '<img src="../pictures/aoe2/age/' + ageImage + '" height="30" />';
+  if (ageImage) {
+    htmlString +=
+        getImageHTML(gamePicturesFolder + 'age/' + ageImage, BO_IMAGE_HEIGHT);
+  }
 
-  htmlBOString += '<img src="../pictures/common/icon/time.png" height="30" />' +
-      currentStep.time;
+  if ('time' in currentStep) {
+    htmlString +=
+        getImageHTML(commonPicturesFolder + 'icon/time.png', BO_IMAGE_HEIGHT) +
+        currentStep.time;
+  }
+  htmlString += '</div></nobr>';
 
-  htmlBOString += '</div></nobr>';
-  htmlBOString +=
-      '<!-- Line separating resources from notes --><hr style="width:100%;text-align:left;margin-left:0"></div>';
+  // Line separating resources from notes
+  htmlString += '<hr style="width:100%;text-align:left;margin-left:0"></div>';
 
-  htmlBOString += '<!-- Notes of the current BO step -->';
+  // Notes of the current BO step
+  const notes = currentStep.notes;
+  const notesCount = notes.length;
 
-  notes = currentStep.notes;
+  for (let noteID = 0; noteID < notesCount; noteID++) {
+    const note = notes[noteID];  // current note line
 
-  for (note of notes) {
-    htmlBOString +=
-        '<nobr><div class="bo_line bo_line_note bo_line_note_first">';
-
-    const timingFlag = false;
-
-    if (timingFlag) {
-      htmlBOString += '<div class="bo_line_note_timing">6:40</div>';
+    // Identify line for CSS properties
+    htmlString += '<nobr><div class="bo_line bo_line_note ';
+    if (noteID == 0) {
+      htmlString += 'bo_line_note_first">';
+    } else if (noteID == notesCount - 1) {
+      htmlString += 'bo_line_note_last">';
+    } else {
+      htmlString += 'bo_line_note_middle">';
     }
 
+    // Add timing indication
+    if (timingFlag && ('time' in currentStep)) {
+      htmlString += '<div class="bo_line_note_timing">' +
+          (noteID == 0 ? currentStep.time : '') + '</div>';
+    }
+
+    // Split note line between text and images
     const splitLine = splitNoteLine(note);
-    splitCount = splitLine.length
+    const splitCount = splitLine.length
 
     if (splitCount > 0) {
       // loop on the line parts
       for (let splitID = 0; splitID < splitCount; splitID++) {
+        // Check if it is a valid image and get its path
         imagePath = getImagePath(splitLine[splitID]);
 
-        if (imagePath) {
-          htmlBOString += '<img src="' + imagePath + '" height="30" />';
-        } else {
-          htmlBOString += splitLine[splitID];
+        if (imagePath) {  // image
+          htmlString += getImageHTML(imagePath, BO_IMAGE_HEIGHT);
+        } else {  // text
+          htmlString += splitLine[splitID];
         }
       }
     }
 
-    htmlBOString += '</div></nobr>';
+    htmlString += '</div></nobr>';
   }
 
-  return htmlBOString;
+  return htmlString;
 }
 
-// Update the overlay content based on the BO design input
+/**
+ * Update the overlay content based on the BO design input.
+ */
 function updateDataBO() {
   const BODesingContent = document.getElementById('bo_design').value;
 
@@ -468,6 +585,12 @@ function updateDataBO() {
     stepCount = dataBO.build_order.length;
     stepID = 0;
     limitStepID();
+
+    if (stepCount < 1) {  // at least one step
+      dataBO = null;
+      stepCount = -1;
+      stepID = -1;
+    }
   } catch (e) {
     dataBO = null;
     stepCount = -1;
@@ -475,11 +598,16 @@ function updateDataBO() {
   }
 }
 
-// Initialize the configuration window
+/**
+ * Initialize the configuration window.
+ */
 function initConfigWindow() {
+  // Initialize the BO panel
   document.getElementById('bo_design').innerHTML = getTemplateBO();
+  updateDataBO();
   updateBOPanel(false);
 
+  // Panel is automatically updated when the BO design panel is changed
   document.getElementById('bo_design')
       .addEventListener('input', function(event) {
         updateDataBO();
@@ -488,22 +616,27 @@ function initConfigWindow() {
 }
 
 /**
- * Update the BO panel rendering
+ * Update the BO panel rendering.
  *
  * @param {bool} overlayFlag    True for overlay, false for configuration
- *     window.
+ *                              window.
  */
 function updateBOPanel(overlayFlag) {
   document.getElementById('bo_panel').innerHTML =
       getBOPanelContent(overlayFlag, stepID);
 }
 
-// Display (and create) the overlay window
+/**
+ * Display (and create) the overlay window.
+ */
 function displayOverlay() {
   // Close window if already open
   if (overlayWindow != null) {
     overlayWindow.close();
   }
+
+  // Check if BO is valid
+  const validBO = checkValidBO();
 
   // Create window
   overlayWindow = window.open('', '_blank', 'width=400, height=200');
@@ -512,29 +645,35 @@ function displayOverlay() {
   const headContent = '<title>RTS Overlay</title>';
 
   // Build order initialized for step 0
-  const bodyContent =
-      '<div id="bo_panel">' + getBOPanelContent(true, 0) + '</div>';
+  const bodyContent = '<div id="bo_panel">' +
+      getBOPanelContent(true, validBO ? 0 : -1) + '</div>';
 
   // HTML content
   let htmlContent = '<!DOCTYPE html><html lang="en">';
 
   htmlContent += '\n<script>';
 
+  htmlContent += '\nconst BO_IMAGE_HEIGHT = ' + BO_IMAGE_HEIGHT + ';';
+  htmlContent += '\nconst ACTION_BUTTON_HEIGHT = ' + ACTION_BUTTON_HEIGHT + ';';
   htmlContent += '\nconst SLEEP_TIME = ' + SLEEP_TIME + ';';
-  htmlContent +=
-      '\nconst dataBO = ' + (dataBO ? JSON.stringify(dataBO) : 'null') + ';';
-  htmlContent += '\nlet stepID = ' + stepID + ';';
-  htmlContent += '\nconst stepCount = ' + stepCount + ';';
 
-  htmlContent += '\n' + limitValue.toString();
+  htmlContent += '\nconst gameName = \'' + gameName + '\';';
+  htmlContent +=
+      '\nconst dataBO = ' + (validBO ? JSON.stringify(dataBO) : 'null') + ';';
+  htmlContent += '\nconst stepCount = ' + (validBO ? stepCount : -1) + ';';
+  htmlContent += '\nlet stepID = ' + (validBO ? 0 : -1) + ';';
+
   htmlContent += '\n' + sleep.toString();
-  htmlContent += '\n' + overlayResizeMove.toString();
+  htmlContent += '\n' + limitValue.toString();
   htmlContent += '\n' + limitStepID.toString();
+  htmlContent += '\n' + overlayResizeMove.toString();
   htmlContent += '\n' + previousStepOverlay.toString();
   htmlContent += '\n' + nextStepOverlay.toString();
   htmlContent += '\n' + splitNoteLine.toString();
   htmlContent += '\n' + checkImageExist.toString();
   htmlContent += '\n' + getImagePath.toString();
+  htmlContent += '\n' + getImageHTML.toString();
+  htmlContent += '\n' + checkValidBO.toString();
   htmlContent += '\n' + getBOPanelContent.toString();
   htmlContent += '\n' + updateBOPanel.toString();
 
@@ -548,8 +687,8 @@ function displayOverlay() {
   // Update overlay HTML content
   overlayWindow.document.write(htmlContent);
 
-  // After a short time (so that elements can be properly updated), adjust the
-  // size of the overlay.
+  // After a short time (so that elements can be properly updated),
+  // adjust the size of the overlay.
   sleep(SLEEP_TIME).then(() => {
     const boPanelOverlay = overlayWindow.document.getElementById('bo_panel');
     const heightOffset = overlayWindow.outerHeight - overlayWindow.innerHeight;
