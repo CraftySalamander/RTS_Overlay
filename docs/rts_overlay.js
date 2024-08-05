33ee72899b20b8e04814bc8d46fc754d52649c49
CraftySalamander <nico.vandernoot+1@gmail.com>
2024-08-05 21:30:00 +0200

    Bring back the download overlay feature

--- docs/rts_overlay.js
+++ docs/rts_overlay.js
@@ -2388,8 +2388,8 @@ function getArrayInstructions(
     'Alternatively, you can click on \'Copy to clipboard\', to copy the build order content, and paste it anywhere.',
     'To load a build order, drag and drop a file with the build order on this panel (or replace the text manually).',
     '',
-    'You can download a copy of the RTS Overlay by clicking the installation button in the URL bar. Firefox users',
-    'may need to install a browser add-on beforehand ("Progressive Web Apps For Firefox" by Filip Štamcar).'
+    'It is highly recommended to download a local copy of RTS Overlay to improve the speed, work offline',
+    'and customize your experience. Hover briefly on \'Download Overlay\' for more information.'
   ];
   return result.concat(validityFontSizeSavePart);
 }
